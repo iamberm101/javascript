67901c202224227a6fd9be7711422228ec71632a
Pognsiri <nextbalanceteam@hotmail.com>
2024-12-17 13:49:02 +0700

    maxChar

---
+++ macChar.js
@@ -0,0 +1,25 @@
+function maxChar(str){
+    const charMap = {};
+    let max = 0;
+    let maxChar = '';
+    for(let char of str){
+        if(charMap[char]){
+            charMap[char] = charMap[char]+ 1;
+        }else{
+            charMap[char] = 1;
+        }
+    }
+
+
+
+    for(const [key,value] of Object.entries(charMap)){
+       if(value > max){
+        max=value;
+        maxChar = key;
+       }
+    }
+
+    return maxChar;
+}
+
+console.log(maxChar("abccccccd"));
